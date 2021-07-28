3ef95e230e1b361f449bc0173e9eb192e67f1f49
NareshPusarla <86452410+NareshPusarla@users.noreply.github.com>
2021-07-28 15:46:51 +0530

    Tc-1.2

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -3,10 +3,10 @@ package com.bridgelabz.regex;
 public class MoodAnalyser {
 	public String message;
 	public void analyseMood(String message) {
-		if(message.equals("“I am in Sad Mood”")) {
+		if(message.equals("I am in Sad Mood")) {
 			System.out.println("SAD");
 		}
-		else {
+		else if(message.equals("I am in Any Mood")) {
 			System.out.println("Happy");
 		}
 	}
