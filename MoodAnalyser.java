962d5f06b05ed608a3c745d2c164e380fde19681
NareshPusarla <86452410+NareshPusarla@users.noreply.github.com>
2021-07-28 15:52:43 +0530

    Refactor tc1

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -2,11 +2,17 @@ package com.bridgelabz.regex;
 
 public class MoodAnalyser {
 	public String message;
+	
+	public MoodAnalyser(String message) {
+		super();
+		this.message = message;
+	}
+
 	public void analyseMood(String message) {
-		if(message.equals("I am in Sad Mood")) {
+		if(this.message.equals("I am in Sad Mood")) {
 			System.out.println("SAD");
 		}
-		else if(message.equals("I am in Any Mood")) {
+		else{
 			System.out.println("Happy");
 		}
 	}
